3baa66c4f8d28472a7d1f6cbc200bc2dcb917418
konglobemeralt <jesper.blidkvist@live.se>
2017-06-09 14:02:46 +0200

    Added some basic java classes

---
+++ Java/src/WorkWithAnimals.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Jesper on 2017-06-09.
+ */
+public class WorkWithAnimals {
+
+    public static void main(String[] args){
+
+        Dog fido = new Dog();
+
+        fido.setName("Fido");
+        System.out.println(fido.getName());
+
+        fido.digHole();
+
+        fido.setWeight(-1);
+
+
+
+    }
+
+    
+}
