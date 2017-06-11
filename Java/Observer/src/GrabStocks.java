813b887156416f7cb99ca6538961d3ed11bd103a
konglobemeralt <jesper.blidkvist@live.se>
2017-06-11 15:44:59 +0200

    Finished observer pattern

--- Java/Observer/src/GrabStocks.java
+++ Java/Observer/src/GrabStocks.java
@@ -8,10 +8,20 @@ public class GrabStocks {
         StockGrabber stockGrabber = new StockGrabber();
 
         StockObserver observer1 = new StockObserver(stockGrabber);
+        StockObserver observer2 = new StockObserver(stockGrabber);
 
         stockGrabber.setIBMPrice(197.00);
         stockGrabber.setAAPLPrice(237.00);
         stockGrabber.setGOOGPrice(567.00);
+
+
+        stockGrabber.unregister(observer1);
+
+        stockGrabber.setIBMPrice(222.00);
+        stockGrabber.setAAPLPrice(233.00);
+        stockGrabber.setGOOGPrice(222.00);
+
+
 
     }
 
