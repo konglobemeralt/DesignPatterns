dcb784fd1240a79caf5859b6caf17d35f9359fcd
konglobemeralt <jesper.blidkvist@live.se>
2017-06-11 15:15:34 +0200

    Added observer

---
+++ Java/Observer/src/Subject.java
@@ -0,0 +1,12 @@
+import java.util.Observer;
+
+/**
+ * Created by Jesper on 2017-06-11.
+ */
+public interface Subject {
+
+    public void register(Observer o);
+    public void unregister(Observer o);
+    public void notifyObserver();
+
+}
