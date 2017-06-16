2a1ab4e87ffcbb77aabe0c7792bda24217537867
konglobemeralt <jesper.blidkvist@live.se>
2017-06-16 13:57:41 +0200

    Added robotPlan

---
+++ Java/Builder/src/RobotPlan.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Jesper on 2017-06-16.
+ */
+public interface RobotPlan {
+
+    public void setRobotHead(String head);
+
+    public void setRobotTorso(String torso);
+
+    public void setRobotArms(String arms);
+
+    public void setRobotLegs(String legs);
+
+
+}
