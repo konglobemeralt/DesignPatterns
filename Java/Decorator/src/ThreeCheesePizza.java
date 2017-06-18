8199849a4111438943bf815c5d8043ffd6038362
konglobemeralt <jesper.blidkvist@live.se>
2017-06-18 14:01:36 +0200

    Added some pizza classes

---
+++ Java/Decorator/src/ThreeCheesePizza.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Jesper on 2017-06-18.
+ */
+public class ThreeCheesePizza implements Pizza {
+
+    private String description = "Mozzarella, Fontina, Parmesan Cheese Pizza";
+    private double cost = 75;
+
+    public void setDescription(String newDescription) {
+
+        description = newDescription;
+
+    }
+
+    public String getDescription() {
+
+        return description;
+
+    }
+
+    public void setCost(double newCost) {
+
+        cost = newCost;
+
+    }
+
+    public double getCost() {
+
+        return cost;
+
+    }
+
+}
