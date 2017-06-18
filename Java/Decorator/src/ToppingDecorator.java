31832dfaceacacaf6f225d8516623a3ae51ac46f
konglobemeralt <jesper.blidkvist@live.se>
2017-06-18 14:04:19 +0200

    Added mozzarella and tomatoe sauce

---
+++ Java/Decorator/src/ToppingDecorator.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Jesper on 2017-06-18.
+ */
+public class ToppingDecorator {
+
+    protected Pizza tempPizza;
+
+    public ToppingDecorator(Pizza newPizza){
+        tempPizza = newPizza;
+    }
+
+    public String getDescription() {
+        return tempPizza.getDescription();
+    }
+
+    public double getCost() {
+        return tempPizza.getCost();
+    }}
