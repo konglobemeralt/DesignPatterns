01749acb2b09a916d6db49d07aa3d5d8eff6d8dc
konglobemeralt <jesper.blidkvist@live.se>
2017-06-19 14:32:00 +0200

    Added basic structure for televisonclass#

---
+++ Java/Command/src/ElectronicDevice.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jesper on 2017-06-19.
+ */
+public interface ElectronicDevice {
+
+    public void on();
+    public void off();
+    public void volumeUp();
+    public void volumeDown();
+}
