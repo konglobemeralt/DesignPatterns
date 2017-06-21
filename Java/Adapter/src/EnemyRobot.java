cd2502e07acdd5e6efdd6ad58224b1986983ba9a
konglobemeralt <jesper.blidkvist@live.se>
2017-06-21 12:14:56 +0200

    Added enemyRobotClass

--- Java/Adapter/src/EnemyRobot.java
+++ Java/Adapter/src/EnemyRobot.java
@@ -1,5 +1,28 @@
+import java.util.Random;
+
 /**
  * Created by Jesper on 2017-06-21.
  */
 public class EnemyRobot {
+
+    Random generator = new Random();
+
+    public void smashWithHands(){
+        int attackDamage = generator.nextInt(10) + 1;
+
+        System.out.println("Enemy Robot Causes " + attackDamage + " Damage with its hands");
+    }
+
+    public void walkForward(){
+        int movement = generator.nextInt(10) + 1;
+
+        System.out.println("Enemy Robot moves " + movement + " units forward");
+    }
+
+    public void reactToHuman(String driverName){
+        System.out.println("Trample " + driverName);
+    }
+
+
+
 }
