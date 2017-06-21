ad6dc67f1c86264a4f59180e7e0a6ea4f4139e3e
konglobemeralt <jesper.blidkvist@live.se>
2017-06-21 12:10:54 +0200

    Added a enemyTank class that uses enemyAttacker interface

--- Java/Adapter/src/EnemyAttacker.java
+++ Java/Adapter/src/EnemyAttacker.java
@@ -2,4 +2,10 @@
  * Created by Jesper on 2017-06-21.
  */
 public interface EnemyAttacker {
+
+    public void fireWeapon();
+    public void driveForward();
+    public void assignDriver(String driverName);
+
+
 }
---
+++ Java/Adapter/src/EnemyTank.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+/**
+ * Created by Jesper on 2017-06-21.
+ */
+public class EnemyTank implements EnemyAttacker {
+
+    Random generator = new Random();
+
+    @Override
+    public void fireWeapon() {
+        int attackDamage = generator.nextInt(10)+1;
+        System.out.println("Enemy Tank Does " + attackDamage);
+    }
+
+    @Override
+    public void driveForward() {
+        int movement = generator.nextInt(5)+1;
+        System.out.println("Enemy Tank moves " + movement);
+    }
+
+    @Override
+    public void assignDriver(String driverName) {
+        System.out.println(driverName + " is driving");
+
+    }
+}
