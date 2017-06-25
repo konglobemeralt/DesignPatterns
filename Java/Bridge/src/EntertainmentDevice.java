86b150f22e5c83e6fd796cdc814852f6fd055495
konglobemeralt <jesper.blidkvist@live.se>
2017-06-25 10:53:52 +0200

    Added bridge pattern project

---
+++ Java/Bridge/src/EntertainmentDevice.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jesper on 2017-06-25.
+ */
+abstract class EntertainmentDevice {
+
+    public int deviceState;
+    public int maxSetting;
+    public int volumeSetting = 0;
+
+}
